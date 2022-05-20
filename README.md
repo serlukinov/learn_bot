7a0fbed85c54eb4e74606deaf1859bca2f12983a
serlukinov <serlukinov@gmail.com>
2022-05-20 07:34:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# learn_bot
+# learn_bot
---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+from time import time 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+
+
+def greet_user(update, context):
+     print("вызван /start")
+     update.message.reply_text("Привет, Привет)))")
+
+def tolk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, tolk_to_me))
+
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
